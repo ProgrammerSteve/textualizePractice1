784b0e0bad17484c85742a26d764b7412b4459e3
Steven <stevenmb1995@gmail.com>
2024-02-13 10:59:47 -0600

    added a reactive attribute for the time display

--- stopwatch.py
+++ stopwatch.py
@@ -1,11 +1,31 @@
+from time import monotonic
+
 from textual import on
 from textual.app import App
 from textual.widgets import Footer, Header, Button, Static
+from textual.reactive import reactive
 from textual.containers import ScrollableContainer
 
 
 class TimeDisplay(Static):
     """Custom time display widget"""
+    time_elapsed = reactive(0)
+
+    def watch_time_elapsed(self):
+        time=self.time_elapsed
+        time, seconds=divmod(time,60)
+        hours,minutes=divmod(time,60)
+        time_string=f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}"
+        self.update(str(time_string))
+    def start(self):
+        """Start keeping track of thetime elapsed."""
+        self.start_time=monotonic()
+    def stop(self):
+        """Stop keeping track of the time elapsed"""
+        self.time_elapsed=monotonic()-self.start_time
+    def reset(self):
+        """Reset time elapsed"""
+
     pass
 
 # When you inherit a widget, you create a widget
@@ -22,11 +42,18 @@ class Stopwatch(Static):
     @on(Button.Pressed, "#start")
     def start_stopwatch(self):
         self.add_class("started")
-        
+        self.query_one(TimeDisplay).start()
+
     @on(Button.Pressed, "#stop")
     def stop_stopwatch(self):
         self.remove_class("started")
-        
+        # queries the child elements
+        self.query_one(TimeDisplay).stop()
+
+    @on(Button.Pressed,"#reset")
+    def reset_stopwatch(self):
+        self.query_one(TimeDisplay).reset()
+
 
 class StopwatchApp(App):
     BINDINGS = [
@@ -46,7 +73,7 @@ class StopwatchApp(App):
         yield Header(show_clock=True)
         yield Footer()
         with ScrollableContainer(id= "stopwatches"):
-            yield Stopwatch(classes="started")
+            yield Stopwatch()
             yield Stopwatch()
             yield Stopwatch()
         # yield ScrollableContainer(

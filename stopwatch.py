e665a58ef398a85d9d93f7abd3d4d1f3fe9def42
Steven <stevenmb1995@gmail.com>
2024-02-13 11:18:59 -0600

    added keybinding to add and remove a stopwatch

--- stopwatch.py
+++ stopwatch.py
@@ -9,7 +9,15 @@ from textual.containers import ScrollableContainer
 
 class TimeDisplay(Static):
     """Custom time display widget"""
+    accumulated_time=0
     time_elapsed = reactive(0)
+    start_time=monotonic()
+
+    def on_mount(self):
+        self.update_timer=self.set_interval(1/60,self.update_time_elapsed,pause=True)
+
+    def update_time_elapsed(self):
+        self.time_elapsed=(monotonic()-self.start_time)+self.accumulated_time
 
     def watch_time_elapsed(self):
         time=self.time_elapsed
@@ -17,14 +25,22 @@ class TimeDisplay(Static):
         hours,minutes=divmod(time,60)
         time_string=f"{hours:02.0f}:{minutes:02.0f}:{seconds:05.2f}"
         self.update(str(time_string))
+
     def start(self):
         """Start keeping track of thetime elapsed."""
         self.start_time=monotonic()
+        self.update_timer.resume()
+
     def stop(self):
         """Stop keeping track of the time elapsed"""
-        self.time_elapsed=monotonic()-self.start_time
+        # self.time_elapsed=monotonic()-self.start_time+self.accumulated_time
+        self.accumulated_time=self.time_elapsed 
+        self.update_timer.pause()
+
     def reset(self):
         """Reset time elapsed"""
+        self.accumulated_time=0
+        self.time_elapsed=0
 
     pass
 
@@ -59,6 +75,9 @@ class StopwatchApp(App):
     BINDINGS = [
         # (key,action name, description),
         ("d","toggle_dark_mode","Toggle dark mode"),
+        ("a","add_stopwatch","Add"),
+        ("r","remove_stopwatch","Remove"),
+
     ]
 
     # CSS="""
@@ -89,6 +108,20 @@ class StopwatchApp(App):
     # It's associated wtih the action called toggle_dark_mode
     def action_toggle_dark_mode(self):
         self.dark=not self.dark
+    def action_add_stopwatch(self):
+        # Create a new instance of the stopwatch
+        stopwatch=Stopwatch()
+        # to mount a widget, select it then call mount
+        container=self.query_one("#stopwatches")
+        container.mount(stopwatch)
+        # scrolls until the stopwatch is visible
+        stopwatch.scroll_visible()
+    def action_remove_stopwatch(self):
+        stopwatches=self.query(Stopwatch)
+        if stopwatches:
+            stopwatches.last().remove()
+
+
 
 if __name__ == "__main__":
     StopwatchApp().run()
